2b8fe8eb42522f7f9c048e235375f6b8dc2bda53
guillaume arthaud <guillaume.arthaud@agence-tbd.com>
2016-12-21 17:22:16 +0100

    hello world

--- registration.php
+++ registration.php
@@ -1,6 +1,7 @@
 <?php
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'GA_HelloWorld',
+    'Guigur_Helloworld',
     __DIR__
 );
